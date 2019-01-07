49fe31bf49c6517b7c986d9077c8b619d87abf11
Kimserey <kimserey.lam@gmail.com>
2019-01-07 12:58:05 +0000

    Added swagger UI

--- hello_flask/__init__.py
+++ hello_flask/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask
+from flasgger import Swagger
 from . import storage
 
 app = Flask(__name__)
+
+swagger = Swagger(app)
+
 storage.create_tables()
--- hello_flask/api.py
+++ hello_flask/api.py
@@ -1,19 +1,35 @@
 from flask import Flask, jsonify, request, g
+from flasgger import swag_from
+from . import swagger
 from marshmallow import Schema, ValidationError, fields, post_load
 
 from . import app
 from . import storage
+
+@swagger.definition('Todo')
+class Todo:
+    """
+    Todo definition
+    ---
+    properties:
+      name:
+        type: string
+        maxLength: 10
+        description: A unique name for the task used as identifier.
+      task:
+        type: string
+        maxLength: 30
+        description: The task to perform.
+    """
+    def __init__(self, name, task):
+        self.name = name
+        self.task = task
 
 ### SCHEMAS ###
 
 def must_not_be_blank(data):
     if not data:
         raise ValidationError('Data not provided.')
-
-class Todo:
-    def __init__(self, name, task):
-        self.name = name
-        self.task = task
 
 class TodoSchema(Schema):
     name = fields.Str(validate=must_not_be_blank)
@@ -39,16 +55,51 @@ def after_request(response):
 
 @app.route("/")
 def home():
+    """
+    Hello endpoint
+    ---
+    description: Hello world.
+    responses:
+      200:
+        description: Hello world.
+    """
     return "Hello, Flask!"
 
 @app.route('/todos/<string:name>')
 def get_todo(name):
+    """
+    Get a todo
+    ---
+    description: Get the todo with the name provided.
+    parameters:
+      - in: path
+        name: name
+        type: string
+        required: true
+    responses:
+      200:
+        description: Return the todo with the name provided.
+    """
     todo = storage.Todo.get_by_id(name)
     result = todo_schema.dump(todo)
     return jsonify(result)
 
 @app.route('/todos/', methods=['POST'])
+@swag_from()
 def post_todo():
+    """
+    Get a todo
+    ---
+    description: Post a new todo.
+    parameters:
+      - name: todo
+        in: body
+        schema:
+            $ref: '#/definitions/Todo'
+    responses:
+      200:
+        description: OK.
+    """
     json_data = request.get_json()
     if not json_data:
         return jsonify({'message': 'No input data provided'}), 400
